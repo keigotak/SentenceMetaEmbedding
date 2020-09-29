7f423bec3c26b230bd13115b4c32a7c4be2dd8dc
keigotak <keigotak@localhost.com>
2020-09-30 00:30:56 +0900

    refactor: add new lines

--- src/examples/SentEval.py
+++ src/examples/SentEval.py
@@ -1,7 +1,7 @@
 import senteval
 from sentence_transformers import SentenceTransformer
-model = SentenceTransformer('roberta-large-nli-stsb-mean-tokens')
 
+model = SentenceTransformer('roberta-large-nli-stsb-mean-tokens')
 params = {'task_path': '/home/keigo/SentEval/data', 'usepytorch': True, 'kfold': 5}
 
 
@@ -9,6 +9,7 @@ def batcher(params, batch):
     sentences = [' '.join(b) for b in batch]
     sentence_embeddings = model.encode(sentences)
     return sentence_embeddings
+
 
 se = senteval.engine.SE(params, batcher)
 transfer_tasks = ['STS12', 'STS13', 'STS14', 'STS15', 'STS16', 'STSBenchmark']
