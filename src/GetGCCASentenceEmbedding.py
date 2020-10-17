fb71e0b59fa965d17f46753c73672eeada72cd1f
keigotak <keigotak@localhost.com>
2020-10-17 19:40:39 +0900

    refactor: modify class to inherit abstract class

--- src/GetGCCASentenceEmbedding.py
+++ src/GetGCCASentenceEmbedding.py
@@ -1,94 +1,57 @@
-import senteval
-import pickle
-from decimal import Decimal, ROUND_HALF_UP
 from gcca.gcca import GCCA
 import numpy as np
+from AbstructGetSentenceEmbedding import *
 
 
-model_names = ['bert-large-nli-stsb-mean-tokens',
-               'distilbert-base-nli-stsb-mean-tokens',
-               'roberta-base-nli-stsb-mean-tokens',
-               'roberta-large-nli-stsb-mean-tokens']
+class GetGCCASentenceEmbedding(AbstructGetSentenceEmbedding):
+    def __init__(self):
+        os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+        super().__init__()
+        self.model_names = ['gcca']
+        self.embeddings = {model_name: {} for model_name in self.model_names}
+        self.output_file_name = 'results.gcca.201017.txt'
+        self.with_reset_output_file = False
+        self.with_save_embeddings = False
 
-gcca_model = GCCA()
-gcca_model.load_params("../models/sts_gcca.h5")
-sentence_embeddings = {}
-for model_name in model_names:
-    with open(f'../models/sentence_embeddings_{model_name}.pkl', 'rb') as f:
-        sentence_embeddings[model_name] = pickle.load(f)
+        self.input_model_names = ['bert-large-nli-stsb-mean-tokens',
+                                  'distilbert-base-nli-stsb-mean-tokens',
+                                  'roberta-base-nli-stsb-mean-tokens',
+                                  'roberta-large-nli-stsb-mean-tokens']
+        self.sentence_embeddings = {}
+        for model_name in self.input_model_names:
+            with open(f'../models/sentence_embeddings_{model_name}.pkl', 'rb') as f:
+                self.sentence_embeddings[model_name] = pickle.load(f)
+
+    def get_model(self):
+        if self.model is None:
+            self.model = GCCA()
+            self.model.load_params("../models/sts_gcca.h5")
+        return self.model
+
+    def batcher(self, params, batch):
+        sentences = [' '.join(sent) for sent in batch]  # To reconstruct sentence from list of words
+
+        # gcca_embeddings = []
+        alist, blist, clist, dlist = [], [], [], []
+        for sentence in sentences:
+            alist.append(self.sentence_embeddings[self.input_model_names[0]][sentence].tolist())
+            blist.append(self.sentence_embeddings[self.input_model_names[1]][sentence].tolist())
+            clist.append(self.sentence_embeddings[self.input_model_names[2]][sentence].tolist())
+            dlist.append(self.sentence_embeddings[self.input_model_names[3]][sentence].tolist())
+
+        alist, blist, clist, dlist = np.array(alist), np.array(blist), np.array(clist), np.array(dlist)
+
+        gcca_embeddings = self.model.transform(alist, blist, clist, dlist)
+
+        return gcca_embeddings[0]
 
 
-def prepare(params, samples):
-    return
-
-
-def batcher(params, batch):
-    sentences = [' '.join(sent) for sent in batch]  # To reconstruct sentence from list of words
-
-    # gcca_embeddings = []
-    alist, blist, clist, dlist = [], [], [], []
-    for sentence in sentences:
-        alist.append(sentence_embeddings[model_names[0]][sentence].tolist())
-        blist.append(sentence_embeddings[model_names[1]][sentence].tolist())
-        clist.append(sentence_embeddings[model_names[2]][sentence].tolist())
-        dlist.append(sentence_embeddings[model_names[3]][sentence].tolist())
-
-    alist, blist, clist, dlist = np.array(alist), np.array(blist), np.array(clist), np.array(dlist)
-
-    gcca_embeddings = gcca_model.transform(alist, blist, clist, dlist)
-
-    return gcca_embeddings[3]
-
-
-model_name = 'gcca'
-params = {'task_path': '/clwork/keigo/SentenceMetaEmbedding/data', 'usepytorch': True}
-params['encoder'] = gcca_model
-
-se = senteval.engine.SE(params, batcher)
-transfer_tasks = ['STS12', 'STS13', 'STS14', 'STS15', 'STS16', 'STSBenchmark']
-results = se.eval(transfer_tasks)
-
-print_header = [model_name, 'pearson-r', 'peason-p_val', 'spearman-r', 'spearman-p_val', 'n_samples']
-print_contents = [print_header]
-
-print_all_header = [model_name, 'pearson-wmean', 'spearman-wmean', 'pearso-mean', 'spearman-wmean']
-print_all_contents = [print_all_header]
-
-for task in results:
-    print_content = []
-    if task == 'STSBenchmark':
-        print_all_contents.append([f'{task}-all',
-                                   f'{Decimal(str(results[task]["pearson"] * 100)).quantize(Decimal("0.00"), rounding=ROUND_HALF_UP) :.2f}',
-                                   f'{Decimal(str(results[task]["spearman"] * 100)).quantize(Decimal("0.00"), rounding=ROUND_HALF_UP) :.2f}',
-                                   '-',
-                                   '-'])
-    else:
-        for category in results[task].keys():
-            if category == 'all':
-                print_all_contents.append([f'{task}-{category}',
-                                           f'{Decimal(str(results[task][category]["pearson"]["wmean"] * 100)).quantize(Decimal("0.00"), rounding=ROUND_HALF_UP) :.2f}',
-                                           f'{Decimal(str(results[task][category]["spearman"]["wmean"] * 100)).quantize(Decimal("0.00"), rounding=ROUND_HALF_UP) :.2f}',
-                                           f'{Decimal(str(results[task][category]["pearson"]["mean"] * 100)).quantize(Decimal("0.00"), rounding=ROUND_HALF_UP) :.2f}',
-                                           f'{Decimal(str(results[task][category]["spearman"]["mean"] * 100)).quantize(Decimal("0.00"), rounding=ROUND_HALF_UP) :.2f}'])
-            else:
-                print_contents.append([f'{task}-{category}',
-                                       f'{Decimal(str(results[task][category]["pearson"][0] * 100)).quantize(Decimal("0.00"), rounding=ROUND_HALF_UP) :.2f}',
-                                       f'{results[task][category]["pearson"][1]}',
-                                       f'{Decimal(str(results[task][category]["spearman"][0] * 100)).quantize(Decimal("0.00"), rounding=ROUND_HALF_UP):.2f}',
-                                       f'{results[task][category]["spearman"][1]}',
-                                       f'{results[task][category]["nsamples"]}'])
-
-with open("../results/results.gcca.201012.txt", "a") as f:
-    for print_all_content in print_all_contents:
-        print(" ".join(["{: >40}"] + ["{: >18}"] * (len(print_all_header) - 1)).format(*print_all_content), file=f)
-
-    print("", file=f)
-
-    for print_content in print_contents:
-        print(" ".join(["{: >40}"] + ["{: >25}"] * (len(print_header) - 2) + ["{: >10}"]).format(*print_content), file=f)
-
-    print("", file=f)
-    print("", file=f)
+if __name__ == '__main__':
+    cls = GetGCCASentenceEmbedding()
+    for model_name in cls.model_names:
+        cls.single_eval(model_name)
+        if cls.with_reset_output_file:
+            cls.with_reset_output_file = False
 
 
 
