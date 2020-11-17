bbb7e1cbfdbfbea48508372ae9f7336289b994af
keigotak <keigotak@localhost.com>
2020-11-18 00:15:27 +0900

    feat: update GCCA

--- src/GCCA.py
+++ src/GCCA.py
@@ -6,45 +6,74 @@ from scipy.linalg import eig
 
 class GCCA:
     def __init__(self):
-        pass
+        self.eta = None
+
+    def centering(self, x):
+        '''
+        :param x: axis0 is number of sentences and axis1 is dimension of each embedding
+        :return: centerized embedding vectors
+        '''
+        avg_by_columns = np.mean(x, axis=0)
+        centered_x = x - avg_by_columns
+        return centered_x
+
+    def get_variance(self, x):
+        std_for_columns = np.std(x, axis=0)
+        return std_for_columns
+
+    def get_covariance_matrics(self, x1, x2):
+        return np.outer(x1, x2)
 
     def fit(self, vectors):
-        avg_vectors = [np.mean(vector) for vector in vectors]
-        div_vectors = [np.std(vector) for vector in vectors]
+        centerized_vectors = [self.centering(vector) for vector in vectors]  # centerized by average for each column
+        div_vectors = [self.get_variance(vector) for vector in vectors]
 
-        cross_vectors_a = [[] for _ in range(vectors.shape[0] * vectors.shape[1])]
-        cross_vectors_b = [[] for _ in range(vectors.shape[0] * vectors.shape[1])]
-        for i in range(vectors.shape[0]):
-            for j in range(vectors.shape[0]):
-                ## calculating cross vectors for each input vector
-                centered_vector_i = vectors[i] - avg_vectors[i]  ## minus average from each input vector
-                centered_vector_j = vectors[j] - avg_vectors[j]  ## minus average from each input vector
-                cross_vectors = np.outer(centered_vector_i, centered_vector_j)  ## calculate cross vector
+        covariance_matrices = []
+        for i in range(len(centerized_vectors)):  # for src1 to calculate covariance matrix
+            for j in range(len(centerized_vectors)):  # for src2 to calculate covariance matrix
+                cm_sum = np.zeros((len(centerized_vectors[i][0]), len(centerized_vectors[j][0])))
+                for k in range(len(centerized_vectors[0])):  # for batch sentences
+                    cm_sum += self.get_covariance_matrics(centerized_vectors[i][k], centerized_vectors[j][k])
+                covariance_matrices.append(cm_sum.tolist())
 
-                ## for stability, adding slight value based on input variance
-                tau = 0.1  ## hyper paramter
-                sigma = div_vectors[i]
-                stability_matrix = tau * sigma * np.eye(cross_vectors.shape[0])
-                cross_vectors += stability_matrix
+        cross_vectors_a = []
+        cross_vectors_b = []
+        flg_new_row = False
+        flg_diagonal = False
+        col, previous_dim = 0, 0
+        for i, cm in enumerate(covariance_matrices):
+            if i % len(vectors) == 0:
+                flg_new_row = True
+            if i % len(vectors) == 0 and i != 0:
+                col += 1
+                previous_dim += len(cm[0])
+            if i % len(vectors) == col:
+                flg_diagonal = True
 
-                ## unpacking covariance matrices, and packing each element of covariance matrices to global 2d matrix
-                ## A is the left and B is the right of eigen equation
-                for k in range(cross_vectors.shape[0]):
-                    if i == j:
-                        cross_vectors_a[cross_vectors.shape[0] * i + k].extend(cross_vectors[k].tolist())
-                        cross_vectors_b[cross_vectors.shape[0] * i + k].extend([0.0] * cross_vectors.shape[1])
+            for j, c in enumerate(cm):
+                if flg_new_row:
+                    if flg_diagonal:
+                        cross_vectors_a.append(c.copy())
+                        cross_vectors_b.append([0.0] * len(c))
                     else:
-                        cross_vectors_a[cross_vectors.shape[0] * i + k].extend([0.0] * cross_vectors.shape[1])
-                        cross_vectors_b[cross_vectors.shape[0] * i + k].extend(cross_vectors[k].tolist())
+                        cross_vectors_a.append([0.0] * len(c))
+                        cross_vectors_b.append(c.copy())
+                else:
+                    if flg_diagonal:
+                        cross_vectors_a[col * previous_dim + j].extend(c)
+                        cross_vectors_b[col * previous_dim + j].extend([0.0] * len(c))
+                    else:
+                        cross_vectors_a[col * previous_dim + j].extend([0.0] * len(c))
+                        cross_vectors_b[col * previous_dim + j].extend(c)
+            flg_new_row = False
+            flg_diagonal = False
 
         ## solving generalized eigen equation
-        # top_k = 2
-        # eigen_values, eigen_vectors = eigs(A=np.array(cross_vectors_a), k=top_k, M=np.array(cross_vectors_b))
         eigen_values, eigen_vectors = eig(a=np.array(cross_vectors_a), b=np.array(cross_vectors_b))
         eigen_values_real_part, eigen_vectors_real_part = eigen_values.real, eigen_vectors.real
 
         ## stacking all eigen vectors
-        eigen_vector_matrix = eigen_vectors_real_part
+        eigen_vector_matrix = eigen_vectors_real_part  ## d1 + d2 * d1 + d2
 
         return eigen_vector_matrix
 
@@ -54,17 +83,11 @@ class GCCA:
 
 if __name__ == '__main__':
     np.random.seed(0)
-    # arr = np.random.randn(2, 10)
-
-    arr = np.array([[1., 0.], [0., 1.]])
-    '''
-    [[ 0.5  0.5  0.5  0.5]
-     [-0.5  0.5 -0.5  0.5]
-     [-0.5  0.5  0.5 -0.5]
-     [ 0.5  0.5 -0.5 -0.5]]
-    '''
+    x1 = np.random.randn(5, 10)
+    x2 = np.random.randn(5, 12)
 
     gcca = GCCA()
-    eigen_vectors = gcca.fit(arr)
+    ret = gcca.fit([x1, x2])
+
     print(eigen_vectors)
     print(eigen_vectors.shape)
