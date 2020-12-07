153df464e295991451fcd5950b7f7bfec7fb5396
keigotak <keigotak@localhost.com>
2020-12-07 23:25:50 +0900

    feat: apply self-implemented GCCA

--- src/FitGCCASentenceEmbedding.py
+++ src/FitGCCASentenceEmbedding.py
@@ -1,32 +1,33 @@
-# https://github.com/rjadr/GCCA
 import pickle
 from pathlib import Path
-from gcca.gcca import GCCA
-import logging
-import numpy as np
 
-# set log level
-# logging.root.setLevel(level=logging.INFO)
-#
-# model_pkls = ["../models/sentence_embeddings_bert-large-nli-stsb-mean-tokens.pkl",
-#               "../models/sentence_embeddings_distilbert-base-nli-stsb-mean-tokens.pkl",
-#               "../models/sentence_embeddings_roberta-base-nli-stsb-mean-tokens.pkl",
-#               "../models/sentence_embeddings_roberta-large-nli-stsb-mean-tokens.pkl"]
-#
-# embeddings = {}
-# for model_pkl in model_pkls:
-#     with Path(model_pkl).open('rb') as f:
-#         embedding = pickle.load(f)
-#     embeddings[model_pkl] = embedding
-#
-# sentences = embedding.keys()
-#
-# vectors = [[] for _ in range(len(model_pkls))]
-# key_to_index = {key: i for i, key in enumerate(embeddings.keys())}
-# for sentence in sentences:
-#     vector = []
-#     for model_type in embeddings.keys():
-#         vectors[key_to_index[model_type]].append(embeddings[model_type][sentence].tolist())
+from GCCA import GCCA
+
+
+model_pkls = ["../models/sentence_embeddings_bert-large-nli-stsb-mean-tokens.pkl",
+              "../models/sentence_embeddings_distilbert-base-nli-stsb-mean-tokens.pkl",
+              "../models/sentence_embeddings_roberta-base-nli-stsb-mean-tokens.pkl",
+              "../models/sentence_embeddings_roberta-large-nli-stsb-mean-tokens.pkl"]
+
+embeddings = {}
+for model_pkl in model_pkls:
+    with Path(model_pkl).open('rb') as f:
+        embedding = pickle.load(f)
+    embeddings[model_pkl] = embedding
+
+sentences = embedding.keys()
+
+vectors = [[] for _ in range(len(model_pkls))]
+key_to_index = {key: i for i, key in enumerate(embeddings.keys())}
+for sentence in sentences:
+    vector = []
+    for model_type in embeddings.keys():
+        tmp = embeddings[model_type][sentence]
+        if type(embeddings[model_type][sentence]) is not list:
+            vectors[key_to_index[model_type]].append(embeddings[model_type][sentence].tolist())
+        else:
+            vectors[key_to_index[model_type]].append(embeddings[model_type][sentence])
+
 #
 # a = np.array(vectors[0])
 # b = np.array(vectors[1])
@@ -46,11 +47,12 @@ import numpy as np
 # # plot
 # gcca.plot_result()
 
-a = np.array([1., 0.])
-b = np.array([0., 1.])
+# a = np.array([1., 0.])
+# b = np.array([0., 1.])
+
 gcca = GCCA()
-gcca.fit(a, b)
-gcca.transform(a, b)
-gcca.plot_result()
+gcca.fit(vectors)
+gcca.save_model()
+gcca.transform(vectors)
 
 
