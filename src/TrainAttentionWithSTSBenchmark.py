1abfe6387412b979cf02773a42cedb8eb6356ade
keigotak <keigotak@localhost.com>
2021-01-24 13:23:36 +0900

    refactor: inherit AbstractTrainer

--- src/TrainAttentionWithSTSBenchmark.py
+++ src/TrainAttentionWithSTSBenchmark.py
@@ -14,16 +14,14 @@ from STSDataset import STSDataset
 from GetHuggingfaceEmbedding import GetHuggingfaceWordEmbedding
 from AttentionModel import MultiheadSelfAttentionModel, AttentionModel
 from AbstractGetSentenceEmbedding import *
+from AbstractTrainer import *
 from ValueWatcher import ValueWatcher
 from DataPooler import DataPooler
 from HelperFunctions import set_seed, get_now
 
 
-class TrainAttentionWithSTSBenchmark:
+class TrainAttentionWithSTSBenchmark(AbstractTrainer):
     def __init__(self):
-        set_seed(0)
-
-        self.datasets = {mode: STSDataset(mode=mode) for mode in ['train', 'dev', 'test']}
         self.model_names = ['bert-base-uncased', 'roberta-base']
         self.source = {model: GetHuggingfaceWordEmbedding(model) for model in self.model_names}
         self.embedding_dims = [self.source[model].model.embeddings.word_embeddings.embedding_dim for model in self.model_names]
@@ -41,11 +39,9 @@ class TrainAttentionWithSTSBenchmark:
         self.gradient_clip = 0.2
         self.weight_decay = 0.0001
         self.parameters = list(self.attention.parameters())
-        self.optimizer = torch.optim.SGD(self.parameters, lr=self.learning_ratio, weight_decay=self.weight_decay)
 
-        self.similarity = lambda s1, s2: np.nan_to_num(cosine(np.nan_to_num(s1), np.nan_to_num(s2)))
+        super().__init__()
 
-        self.tag = get_now()
         self.save_model_path = f'../models/attention-{self.tag}.pkl'
         self.information_file = f'../results/attention/info-{self.tag}.txt'
 
@@ -98,92 +94,6 @@ class TrainAttentionWithSTSBenchmark:
 
         return pooled_sentence_embedding, attention_weight
 
-    def train_epoch(self, with_pbar=False):
-        mode = 'train'
-        if with_pbar:
-            pbar = tqdm(total=self.datasets[mode].dataset_size)
-
-        ## batch loop
-        while not self.datasets[mode].is_batch_end():
-            sentences1, sentences2, scores = self.datasets[mode].get_batch()
-
-            ## get vector representation for each embedding and batch data
-            batch_embeddings = []
-            with torch.no_grad():
-                for sent1, sent2 in zip(sentences1, sentences2):
-                    embeddings = {}
-                    for model_name in self.model_names:
-                        rets = self.source[model_name].get_word_embeddings(sent1, sent2)
-                        if False:
-                            print('\t'.join([' '.join(items) for items in rets['tokens']]))
-                        embeddings[model_name] = rets['embeddings']
-                    batch_embeddings.append(embeddings)  ## batch, embedding type, sentence source, sentence length, hidden size
-
-            ## get attention output
-            _, _, _ = self.batch_step(batch_embeddings, scores, with_training=True)
-
-            if with_pbar:
-                pbar.update(self.datasets[mode].batch_size)
-
-            # print(str(self.datasets[mode]) + f' loss: {running_loss}')
-
-        if with_pbar:
-            pbar.close()
-
-    def train(self, num_epoch=10):
-        vw = ValueWatcher()
-        for i in range(num_epoch):
-            self.train_epoch()
-            self.datasets['train'].reset(with_shuffle=True)
-            rets = self.inference('dev')
-
-            vw.update(rets['pearson'][0])
-            if vw.is_max():
-                trainer.save_model()
-
-    def inference(self, mode='dev'):
-        running_loss = 0.0
-        results = {}
-        sys_scores, gs_scores = [], []
-
-        # batch loop
-        while not self.datasets[mode].is_batch_end():
-            sentences1, sentences2, scores = self.datasets[mode].get_batch()
-
-            # get vector representation for each embedding
-            batch_embeddings = []
-            with torch.no_grad():
-                for sent1, sent2 in zip(sentences1, sentences2):
-                    embeddings = {}
-                    for model_name in self.model_names:
-                        rets = self.source[model_name].get_word_embeddings(sent1, sent2)
-                        if False:
-                            print('\t'.join([' '.join(items) for items in rets['tokens']]))
-                        embeddings[model_name] = rets['embeddings']
-                    batch_embeddings.append(embeddings)  ## batch, embedding type, sentence source, sentence length, hidden size
-
-                # get attention output
-                gs, sys, loss = self.batch_step(batch_embeddings, scores, with_calc_similality=True)
-                sys_scores.extend(sys)
-                gs_scores.extend(gs)
-                running_loss += loss
-
-        results = {'pearson': pearsonr(sys_scores, gs_scores),
-                   'spearman': spearmanr(sys_scores, gs_scores),
-                   'nsamples': len(sys_scores)}
-
-        print_contents = [f'STSBenchmark-{mode}',
-                          f'pearson: {self.get_round_score(results["pearson"][0]) :.2f}',
-                          f'spearman: {self.get_round_score(results["spearman"][0]) :.2f}']
-        results['prints'] = print_contents
-
-        print(f'[{mode}] ' + str(self.datasets[mode]) + f' loss: {running_loss}')
-        print(' '.join(print_contents))
-
-        self.datasets[mode].reset()
-
-        return results
-
     def save_model(self):
         torch.save(self.attention.state_dict(), self.save_model_path)
         self.save_information_file()
@@ -194,21 +104,8 @@ class TrainAttentionWithSTSBenchmark:
         else:
             self.attention.load_state_dict(torch.load(self.save_model_path))
 
-    def get_round_score(self, score):
-        return Decimal(str(score * 100)).quantize(Decimal("0.00"), rounding=ROUND_HALF_UP)
-
-    def append_information_file(self, results):
-        information_file = Path(self.information_file)
-        if not information_file.parent.exists():
-            information_file.parent.mkdir(exist_ok=True)
-
-        with information_file.open('a') as f:
-            f.write('\n'.join(results))
-
     def save_information_file(self):
-        information_file = Path(self.information_file)
-        if not information_file.parent.exists():
-            information_file.parent.mkdir(exist_ok=True)
+        super().save_information_file()
 
         with information_file.open('w') as f:
             f.write(f'source: {",".join(self.model_names)}\n')
