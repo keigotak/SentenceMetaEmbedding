a4d0fe4d3cc71af46ef785943515d8d5962c27e0
keigotak <keigotak@localhost.com>
2021-01-26 23:55:00 +0900

    feat: add function of get torch.device

--- src/HelperFunctions.py
+++ src/HelperFunctions.py
@@ -1,8 +1,10 @@
+import os
 import random
+from datetime import datetime
+
 import numpy as np
 import torch
 
-from datetime import datetime
 
 def set_seed(seed=0):
     random.seed(seed)
@@ -12,3 +14,18 @@ def set_seed(seed=0):
 def get_now():
     now = datetime.now()
     return now.strftime("%m%d%Y%H%M%S%f")
+
+def get_device(device):
+    if type(device) == torch.device:
+        return device
+
+    if device == 'cpu':
+        return torch.device('cpu')
+    else:
+        if type(device) != int:
+            device = int(device)
+        if torch.cuda.is_available():
+            os.environ["CUDA_VISIBLE_DEVICES"] = f"{device}"
+            return torch.device(device)
+        else:
+            return torch.device('cpu')
